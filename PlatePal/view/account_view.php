ec2874a03dbb977a9a18c384a28400d5d1cc938d
lena <lena.sarieva@gmail.com>
2024-11-05 16:39:43 -0700

    Delete PlatePal directory

--- PlatePal/view/account_view.php
+++
@@ -1,7 +0,0 @@
-<?php include('header.php'); ?>
-<main>
-    <h2>Welcome, <?php echo htmlspecialchars($_SESSION['username']); ?></h2>
-    <p>Your Saved Recipes:</p>
-    <!-- Display saved recipes -->
-</main>
-<?php include('footer.php'); ?>
--- PlatePal/view/index_view.php
+++
@@ -1,9 +0,0 @@
-<?php include('header.php'); ?>
-<main>
-    <h2>Find Your Perfect Recipe</h2>
-    <form action="../controller/search_controller.php" method="post">
-        <input type="text" name="search_term" placeholder="Search by ingredient or meal type">
-        <button type="submit" name="search">Search</button>
-    </form>
-</main>
-<?php include('footer.php'); ?>
